2ea7f75c6a76010741b2cb91ca7d1f34bda1f74e
simonwardjones <simon.ward-jones@carwow.co.uk>
2019-07-05 13:28:15 +0100

    refactor to CRA format, and change syntax highlight component

--- src/RecipeResult.js
+++ src/RecipeResult.js
@@ -3,8 +3,10 @@ import PropTypes from 'prop-types';
 import './RecipeResult.css';
 import { ListGroupItem , ListGroupItemHeading, ListGroupItemText} from 'reactstrap';
 import { Badge, Collapse, Button, CardBody, Card, Col, Row } from 'reactstrap';
-import { PrismCode } from 'react-prism';
 import Clipboard from 'clipboard';
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { okaidia } from 'react-syntax-highlighter/dist/esm/styles/prism';
+
 
 class RecipeResult extends React.Component {
   componentDidMount() {
@@ -55,9 +57,9 @@ class RecipeResult extends React.Component {
           <path d="M17,9H7V7H17M17,13H7V11H17M14,17H7V15H14M12,3A1,1 0 0,1 13,4A1,1 0 0,1 12,5A1,1 0 0,1 11,4A1,1 0 0,1 12,3M19,3H14.82C14.4,1.84 13.3,1 12,1C10.7,1 9.6,1.84 9.18,3H5A2,2 0 0,0 3,5V19A2,2 0 0,0 5,21H19A2,2 0 0,0 21,19V5A2,2 0 0,0 19,3Z" />
           </svg>
           </Button>
-          <PrismCode component="pre" className="language-python" id="target_copy">
+          <SyntaxHighlighter language="python" style={okaidia}>
           {this.props.recipeData.snippet}
-          </PrismCode> 
+          </SyntaxHighlighter> 
           </div>
         </Collapse>
       </ListGroupItem>
